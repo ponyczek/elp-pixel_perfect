a6ede3084c72cf6450d5628886f00f5421a59443
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-10-27 18:38:28 +0200

    Simple data mockup

---
+++ assets/js/dataMockup.js
@@ -0,0 +1,62 @@
+var issues = [
+  {
+    day: "19-07-2016",
+    issues: [
+      {
+        title: "Page changes",
+        star: true
+      },
+      {
+        title: "Review of last issues",
+        star: true
+      }
+    ]
+  },
+  {
+    day: "18-07-2016",
+    issues: [
+      {
+        title: "Visual UI Update Review",
+        star: false
+      },
+      {
+        title: "Sidebar changes",
+        star: false
+      }
+    ]
+  },
+  {
+    day: "15-07-2016",
+    issues: [
+      {
+        title: "Crash update",
+        star: false
+      },
+      {
+        title: "Page visual UI Update Review",
+        star: true
+      },
+      {
+        title: "Sidebar update",
+        star: false
+      }
+    ]
+  },
+  {
+    day: "14-07-2016",
+    issues: [
+      {
+        title: "Crash issue",
+        star: true
+      },
+      {
+        title: "Visua update & Crash resolve",
+        star: true
+      },
+      {
+        title: "Extra Issue for nicer Scroll",
+        star: false
+      }
+    ]
+  }
+];
